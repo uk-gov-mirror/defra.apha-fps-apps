48f0fb5bea82a3bc373e4479dad078d47a12d6e4
Kamlesh Daxini <kamlesh.daxini@atos.ai>
2026-06-08 11:58:36 +0530

    feat : create one view via changeset and liquibase default schema setting

---
+++ dbscript/liquibase/changesets/002_create_fps_vfps_totals.sql
@@ -0,0 +1,37 @@
+--liquibase formatted sql
+
+--changeset repo-admin:002_fps.vfps_totals labels:ddl context:all
+
+
+CREATE OR REPLACE VIEW fps.vfps_totals
+ AS
+ SELECT fpsyeartotals.parentproject,
+    fpsyeartotals.program,
+    fpsyeartotals.totaladditionalcosts,
+    fpsyeartotals.totalanimalcosts,
+    fpsyeartotals.totalstaffcosts,
+    fpsyeartotals.totaltestcosts,
+    fpsyeartotals.totalcosts,
+    fpsyeartotals.custincome,
+    fpsyeartotals.transferincome,
+    fpsyeartotals.totalincome,
+    fpsyeartotals.budget_cvl,
+    fpsyeartotals.requiredprofit,
+    fpsyeartotals.manager,
+    fpsyeartotals.customer,
+    fpsyeartotals.projectstatus,
+    fpsyeartotals.pvsincome,
+    fpsyeartotals.plancaseworkdebit,
+    ma_a.bfbudget AS ma_bfbudget,
+    fpsyeartotals.fpsyear
+   FROM fps.fpsyeartotals
+     LEFT JOIN ( SELECT my_tlkpprojectradtrackdata.project,
+            my_tlkpprojectradtrackdata.bfbudget
+           FROM mabarchive.my_tlkpprojectradtrackdata
+          WHERE my_tlkpprojectradtrackdata.year::text = (( SELECT "right"(tbldb_variables.db_var_value::text, 4) AS "right"
+                   FROM fps.tbldb_variables
+                  WHERE tbldb_variables.db_var_name::text = 'DB_Name'::text))) ma_a ON fpsyeartotals.parentproject::text = ma_a.project::text;
+
+ALTER TABLE fps.vfps_totals
+    OWNER TO fpsdev;
+
