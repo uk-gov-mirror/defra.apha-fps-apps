3706a4780d6aa4b10d5fc97ba417999f31692e19
Kamlesh Daxini <kamlesh.daxini@atos.ai>
2026-06-17 15:53:32 +0530

    feat : Rename col in view qrytotaltestcosts and add post restore seed data for tblyearmaster

--- dbscript/liquibase/changesets/008_Modify_fps_qrytotaltestcosts.sql
+++ dbscript/liquibase/changesets/008_Modify_fps_qrytotaltestcosts.sql
@@ -1,1 +1,13 @@
-CREATE OR REPLACE VIEW fps.qrytotaltestcosts AS SELECT tr.jobcode, p.fpsyear, SUM(tr.notests * tr.testprice) AS totaltestcosts FROM fps.vtbltestrequ tr INNER JOIN fps.tlkpproject p ON p.parentproject = tr.jobcode GROUP BY tr.jobcode, p.fpsyear;
+
+--liquibase formatted sql
+
+--changeset repo-admin:008_fps_qrytotaltestcosts labels:ddl context:all
+
+DROP VIEW fps.qrytotaltestcosts;
+CREATE OR REPLACE VIEW fps.qrytotaltestcosts AS 
+SELECT tr.jobcode, p.fpsyear, SUM(tr.notests * tr.testprice) AS totaltestcosts 
+FROM fps.vtbltestrequ tr INNER JOIN fps.tlkpproject p ON p.parentproject = tr.jobcode 
+GROUP BY tr.jobcode, p.fpsyear;
+
+--ROLLBACK 
+-- Not applicable
