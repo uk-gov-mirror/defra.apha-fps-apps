51db28bf896bf9b794e6fc88e09fae1c8a40d119
Kamlesh Daxini <kamlesh.daxini@atos.ai>
2026-06-18 08:02:53 +0530

    feat : Modified 2 views as per excel req CR013 and CR014

--- dbscript/liquibase/changesets/008_Modify_fps_qrytotaltestcosts.sql
+++ dbscript/liquibase/changesets/008_Modify_fps_qrytotaltestcosts.sql
@@ -1,13 +1,18 @@
-
 --liquibase formatted sql
 
 --changeset repo-admin:008_fps_qrytotaltestcosts labels:ddl context:all
 
 DROP VIEW fps.qrytotaltestcosts;
-CREATE OR REPLACE VIEW fps.qrytotaltestcosts AS 
-SELECT tr.jobcode, p.fpsyear, SUM(tr.notests * tr.testprice) AS totaltestcosts 
-FROM fps.vtbltestrequ tr INNER JOIN fps.tlkpproject p ON p.parentproject = tr.jobcode 
-GROUP BY tr.jobcode, p.fpsyear;
+CREATE OR REPLACE VIEW fps.qrytotaltestcosts AS
+SELECT
+    tr.jobcode,
+    tr.fpsyear,
+    SUM(tr.notests * tr.testprice) AS totaltestcosts
+FROM fps.vtbltestrequ tr
+JOIN fps.tlkpproject p
+    ON p.parentproject = tr.jobcode
+   AND p.fpsyear = tr.fpsyear
+GROUP BY tr.jobcode, tr.fpsyear;
 
---ROLLBACK 
--- Not applicable
+--ROLLBACK
+--Not Applicable
